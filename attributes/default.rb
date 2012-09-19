db7fd131cf59fcfc7717971c5c79d80e77c13e62
Samir ASNOUN <samir.asnoun@gmail.com>
2012-09-19 11:49:21 +0200

    change the cloudfoundry health manager recipe for it seeks nats and cloudcontroller data

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,43 +1,43 @@
 include_attribute "cloudfoundry-common"
 
 # The Health Manager's log level.
-default[:cloudfoundry_health_manager][:log_level] = "info"
+default['cloudfoundry_health_manager']['log_level'] = "info"
 
 # Where to write the Health Manager's logs.
-default[:cloudfoundry_health_manager][:log_file] = File.join(node[:cloudfoundry_common][:log_dir], "health_manager.log")
+default['cloudfoundry_health_manager']['log_file'] = File.join(node['cloudfoundry_common']['log_dir'], "health_manager.log")
 
 # Where to write the Health Manager's pid file.
-default[:cloudfoundry_health_manager][:pid_file] = File.join(node[:cloudfoundry_common][:pid_dir], "health_manager.pid")
+default['cloudfoundry_health_manager']['pid_file'] = File.join(node['cloudfoundry_common']['pid_dir'], "health_manager.pid")
 
-# TODO (trotter): Find out what this does.
-default[:cloudfoundry_health_manager][:database_scan]    = 60
+# TODO (trotter) Find out what this does.
+default['cloudfoundry_health_manager']['database_scan']    = 60
 
-# TODO (trotter): Find out what this does.
-default[:cloudfoundry_health_manager][:droplet_lost]     = 30
+# TODO (trotter) Find out what this does.
+default['cloudfoundry_health_manager']['droplet_lost']     = 30
 
-# TODO (trotter): Find out what this does.
-default[:cloudfoundry_health_manager][:droplet_analysis] = 10
+# TODO (trotter) Find out what this does.
+default['cloudfoundry_health_manager']['droplet_analysis'] = 10
 
-# TODO (trotter): Find out what this does.
-default[:cloudfoundry_health_manager][:flapping_death]   = 3
+# TODO (trotter) Find out what this does.
+default['cloudfoundry_health_manager']['flapping_death']   = 3
 
-# TODO (trotter): Find out what this does.
-default[:cloudfoundry_health_manager][:flapping_timeout] = 180
+# TODO (trotter) Find out what this does.
+default['cloudfoundry_health_manager']['flapping_timeout'] = 180
 
-# TODO (trotter): Find out what this does.
-default[:cloudfoundry_health_manager][:restart_timeout]  = 20
+# TODO (trotter) Find out what this does.
+default['cloudfoundry_health_manager']['restart_timeout']  = 20
 
-# TODO (trotter): Find out what this does.
-default[:cloudfoundry_health_manager][:stable_state]     = 60
+# TODO (trotter) Find out what this does.
+default['cloudfoundry_health_manager']['stable_state']     = 60
 
-#TODO (ASNOUN):Paramters need to connect to the database  
-default[:cloudfoundry_health_manager][:database_host ] = ""
-default[:cloudfoundry_health_manager][:database_name]=""
-default[:cloudfoundry_health_manager][:postgres_password]=""
+#TODO (ASNOUN)Paramters need to connect to the database  
+default['cloudfoundry_health_manager']['database_host '] = ""
+default['cloudfoundry_health_manager']['database_name']=""
+default['cloudfoundry_health_manager']['postgres_password']=""
 
-#TODO (ASNOUN):Paramters need to connect to the nats
-default[:cloudfoundry_health_manager][:nats_user]=""
-default[:cloudfoundry_health_manager][:nats_password]=""
-default[:cloudfoundry_health_manager][:nats_host]=""
-default[:cloudfoundry_health_manager][:nats_port]=""
+#TODO (ASNOUN)Paramters need to connect to the nats
+default['cloudfoundry_health_manager']['nats_user']=""
+default['cloudfoundry_health_manager']['nats_password']=""
+default['cloudfoundry_health_manager']['nats_host']=""
+default['cloudfoundry_health_manager']['nats_port']=""
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,6 @@
 #
-# Cookbook Name:: cloudfoundry-health_manager
-# Recipe:: default
+# Cookbook Name cloudfoundry-health_manager
+# Recipe default
 #
 # Copyright 2012, Trotter Cashion
 #
@@ -8,7 +8,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#     http//www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,18 +16,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+if Chef::Config[:solo]
+ Chef::Log.warn("This recipe uses search. Chef Solo does not support search.")
 
-m_nodes = search(:node, "role:cloudfoundry_controller")
-m_node = m_nodes.first
+else 
 
-node.set[:cloudfoundry_health_manager][:database_host ] = m_node.ipaddress
-node.set[:cloudfoundry_health_manager][:database_name] = m_node.cloudfoundry_cloud_controller.database.name
-node.set[:cloudfoundry_health_manager][:postgres_password]= m_node.postgresql.password.postgres
+  m_nodes = search(:node, "role:cloudfoundry_controller")
+  m_node = m_nodes.first
+  if m_nodes.count > 0 
+    node.set['cloudfoundry_health_manager']['database_host '] = m_node.ipaddress
+    node.set['cloudfoundry_health_manager']['database_name'] = m_node.cloudfoundry_cloud_controller.database.name
+    node.set['cloudfoundry_health_manager']['postgres_password']= m_node.postgresql.password.postgres
+  end 
 
-node.set[:cloudfoundry_health_manager][:nats_user]= m_node.nats_server.user
-node.set[:cloudfoundry_health_manager][:nats_password]= m_node.nats_server.password
-node.set[:cloudfoundry_health_manager][:nats_host]= m_node.ipaddress
-node.set[:cloudfoundry_health_manager][:nats_port]= m_node.nats_server.port
+  nats_nodes = search(:node, "role:cloudfoundry_nats_server")
+  nats_node_first = nats_nodes.first
+  if nats_nodes.count > 0  
+    node.set['cloudfoundry_health_manager']['nats_user']= nats_node_first.nats_server.user
+    node.set['cloudfoundry_health_manager']['nats_password']= nats_node_first.nats_server.password
+    node.set['cloudfoundry_health_manager']['nats_host']= nats_node_first.ipaddress
+    node.set['cloudfoundry_health_manager']['nats_port']= nats_node_first.nats_server.port
+  end 
 
+end
 
 cloudfoundry_component "health_manager"
+
+
